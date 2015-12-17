b35561f00c59f2c7f6dad078a07c5ee5565178f7
董凯 <179367003@qq.com>
2015-12-17 10:51:36 +0800

    add test

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -5,7 +5,7 @@
 
 from scrapy.item import Item, Field
 
-class TutorialItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+class DmozItem(Item):
+    title = Field()
+    link = Field()
+    desc = Field()
